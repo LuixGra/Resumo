557df526962b7904a3d52176edc705541df1f49b
LuixGra <luizngraceli@gmail.com>
2024-03-24 21:31:36 -0300

    New version
    new and improved

---
+++ projetos/srcipt.js
@@ -0,0 +1,8 @@
+/*<ul type="square">
+                <li><a href="Ex001.html" rel="next">Minha primeira página.</a></li>
+                <li><a href="Seletor de Idade/index.html" rel="next">Seletor de idade.</a></li>
+                <li><a href="amanheceu/index.html" rel="next">Relogio Manhã, Tarde, Noite.</a></li>
+                <li><a href="Tabuada/index.html" rel="next">Tabuada.</a></li>
+                <li><a href="to-do list/index.html" rel="next">To-Do List.</a></li>
+            </ul>*/
+
